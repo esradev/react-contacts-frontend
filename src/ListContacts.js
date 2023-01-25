1f1e89be7d57a807aeaad76285c427c6fd723866
Wpstorm <wpstormdev@gmail.com>
2023-01-25 09:20:09 +0330

    Created search functionality for our app. Display our live search results.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,38 +1,86 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import escapeStringRegexp from "escape-string-regexp";
+import sortBy from "sort-by";
 
-function ListContacts(props) {
-  return (
-    <ol className="contact-list">
-      {props.contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`,
-            }}
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    removeContact: PropTypes.func.isRequired,
+  };
+
+  state = {
+    query: "",
+  };
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() });
+  };
+
+  clearQuery = () => {
+    this.setState({ query: "" });
+  };
+
+  render() {
+    const { contacts, removeContact } = this.props;
+    const { query } = this.state;
+
+    let showingContacts;
+    if (query) {
+      const match = new RegExp(escapeStringRegexp(query, "i"));
+      showingContacts = contacts.filter((contact) => match.test(contact.name));
+    } else {
+      showingContacts = contacts;
+    }
+
+    showingContacts.sort(sortBy("name"));
+
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
+        </div>
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now showing {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={this.clearQuery}>Show all</button>
           </div>
-          <button
-            className="contact-remove"
-            onClick={() => {
-              props.removeContact(contact);
-            }}
-          >
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
-  );
+        )}
+        <ol className="contact-list">
+          {showingContacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`,
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => {
+                  removeContact(contact);
+                }}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  removeContact: PropTypes.func.isRequired,
-};
 
 export default ListContacts;
