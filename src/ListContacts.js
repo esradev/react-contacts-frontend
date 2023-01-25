66a6b1f8c375c59aa533b94306923cb8740572e3
Wpstorm <wpstormdev@gmail.com>
2023-01-25 08:28:02 +0330

    Maked remove contact button work, and used PropTypes package for validate intended data types in our React app.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class ListContacts extends Component {
-  render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map((contact) => (
-          <li key={contact.id} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`,
-              }}
-            />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className="contact-remove">Remove</button>
-          </li>
-        ))}
-      </ol>
-    );
-  }
+function ListContacts(props) {
+  return (
+    <ol className="contact-list">
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+          />
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button
+            className="contact-remove"
+            onClick={() => {
+              props.removeContact(contact);
+            }}
+          >
+            Remove
+          </button>
+        </li>
+      ))}
+    </ol>
+  );
 }
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  removeContact: PropTypes.func.isRequired,
+};
 
 export default ListContacts;
